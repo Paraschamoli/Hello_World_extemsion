2933570a5885fca50b5063a052d2c6135c7d191f
paras chamoli <paraschamoli2592002@gmail.com>
2024-10-03 11:39:05 +0530

    first commmit

---
+++ popup.js
@@ -0,0 +1,11 @@
+document.getElementById("getTitle").addEventListener("click", function () {
+  console.log("Button clicked!"); // Debugging line
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (tabs.length > 0) {
+      const currentTab = tabs[0];
+      document.getElementById("title").textContent = currentTab.title;
+    } else {
+      document.getElementById("title").textContent = "No active tab found.";
+    }
+  });
+});
